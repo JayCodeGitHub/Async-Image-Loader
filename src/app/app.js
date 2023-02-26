89af2341c80bb66e2976d90706d1a7b37676b047
JayCode Developer <JayCode.contakt@gmail.com>
2023-02-26 22:15:31 +0100

    Added Elements by JS Script

--- src/app/app.js
+++ src/app/app.js
@@ -3,11 +3,12 @@ import Image from"../assets/Image.jpeg"
 export function run() {
 
     const posts = document.getElementById("posts")
-
-    const post = document.createElement('section');
-    const img = document.createElement('img');
-    img.src = Image
-
-    post.appendChild(img);
-    posts.appendChild(post);
+    
+    for(let i=0;i<5;i++){
+        const post = document.createElement('section');
+        const img = document.createElement('img');
+        img.src = Image
+        posts.appendChild(post);
+        post.appendChild(img);
+    }
 }
