47f4360e27ac1c229b6d12ad75a3f9c71d972d52
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-05 15:53:08 +0100

    Added async images Loader

--- src/app/image.js
+++ src/app/image.js
@@ -1,12 +1,20 @@
-import Image from"../assets/Image.jpeg"
-
 export function run() {
 
-    const posts = document.getElementById("posts")
-    
-    const post = document.createElement('section');
-    const img = document.createElement('img');
-    img.src = Image
-    posts.appendChild(post);
-    post.appendChild(img);
+    const  imageLoader = async() => {
+        try {
+            const posts = document.getElementById("posts")
+            const post = document.createElement('section');
+            const img = document.createElement('img');
+            const response = await fetch('https://api.thecatapi.com/v1/images/search')
+            const data = await response.json();
+            const Image = data[0].url;
+            img.src = Image
+            posts.appendChild(post);
+            post.appendChild(img);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+        imageLoader();
+    console.log("console log before image load")
 }
